cf520438788dace289c8d01a53b5f84f30d0c201
innovator <vedanthpc98@gmail.com>
2024-05-02 15:10:12 +0530

    updates #17

--- Array/arraylist.java
+++ Array/arraylist.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 public class arraylist {
     public static void main(String args[]) {
-        ArrayList<Integer> al = new ArrayList<>();
-        al.add(10);
+        ArrayList<Integer> al = new ArrayList<Integer>();
         al.add(20);
         al.add(30);
         al.add(40);
+        al.add(0, 10); // Adds element to index 0
 
         al.remove(2); // Removes element from index 2
 
